4f79cb78d152cdf1f72c2ce4b8f4bc8cacb653d1
robincpc <chienpinchen@gmail.com>
2016-03-19 17:19:33 -0400

    Use K-means to cluster foods need to protect, add power capsule in foodlist Use food ate by enemies to locate the position of enemy. modify wieghting of evaluation (patrol around food and enemy). let agent switch roles (defender, invader) after dead.

--- contest/myTeam.py
+++ contest/myTeam.py
@@ -41,10 +41,17 @@ def createTeam(firstIndex, secondIndex, isRed,
 # functions for the extended classes to use. 
 class BaseBehaviorAgent(CaptureAgent):
   # Class vairable
-  ProFoods = []
+  
   FoodList = []
   FoodGrp0 = []
   FoodGrp1 = []
+  
+  ProFoods = []  
+  ProFoodGrp0 = []
+  ProFoodGrp1 = []
+  ProFoodGrp2 = []
+  num_dead = [0, 0, 0, 0]
+  pre_pole = [None, None, None, None]   # record the role of agent is previous move
 
   # This method handles the initial setup of the
   # agent to populate useful fields (such as what team
@@ -63,9 +70,22 @@ class BaseBehaviorAgent(CaptureAgent):
     #global ProFoods, FoodList, FoodGrp0, FoodGrp1        # may not a good way to share variable between objects
     BaseBehaviorAgent.ProFoods = self.getFoodYouAreDefending(gameState).asList()
     BaseBehaviorAgent.FoodList = self.food(gameState)
+    self.caps = self.getCapsules(gameState)
+    
+    self.cur_pro_food = None        # intial a variable to store the position of food for patroling
+    self.cur_pro_grp = 0            # start patrol from group 1 
+    
+    self.ini_pos = gameState.getAgentPosition(self.index)
+    
+    self.num_actions = 0            # count how many move agent did
+    BaseBehaviorAgent.num_dead = [0,0,0,0]
+    
+    #self.pre_role = None            # record the role of agent is previous move
     
     if self.index < 2:
-        self.doKmeans(gameState)
+        self.doKmeans(gameState)    # do K-means for foods agents want to eat
+    else:
+        self.doKmeans3(gameState)   # do K-means for foofs agents need to protect
     
   
   def doKmeans(self, gameState):
@@ -90,7 +110,7 @@ class BaseBehaviorAgent(CaptureAgent):
     MAX_ITER = 10
     b_converge = False
     
-    print cen_pnts
+    #print cen_pnts
     while n_iter < MAX_ITER and not b_converge:
       for food in BaseBehaviorAgent.FoodList:
         dst0 = util.manhattanDistance(cen_pnts[0], food) #self.getMazeDistance(cen_pnts[0], food)
@@ -112,12 +132,17 @@ class BaseBehaviorAgent(CaptureAgent):
       cent1y = sum([i[1] for i in self.foodGrp1])/len(self.foodGrp1)
       
       if cen_pnts[0] == (int(cent0x), int(cent0y)) and cen_pnts[1] == (int(cent1x), int(cent1y)):
-        print "# of iter: ", n_iter+1
+        #print "# of iter: ", n_iter+1
         b_converge = True
+        for c in self.caps:
+            if util.manhattanDistance(cen_pnts[0], c) < util.manhattanDistance(cen_pnts[1], c):
+                self.foodGrp0.append(c)
+            else:
+                self.foodGrp1.append(c)
       else:
         cen_pnts[0] = (int(cent0x), int(cent0y))
         cen_pnts[1] = (int(cent1x), int(cent1y))
-      print cen_pnts
+      #print cen_pnts
       n_iter += 1
       
     BaseBehaviorAgent.FoodGrp0 = self.foodGrp0
@@ -127,6 +152,89 @@ class BaseBehaviorAgent(CaptureAgent):
     for j in self.foodGrp1:
         self.debugDraw(j, [0,1,0])
    
+  def doKmeans3(self, gameState):
+    ''' use k-means to clauster food in three group, let agent patrol for diff food groups'''
+    #wall_list = gameState.getWalls().asList()
+    cen_pnts=[]  # list to store centroids
+    
+    # random pick two centroid point
+    for i in xrange(3):
+        seed = random.randint(1,10)
+        centroid = random.Random(seed).choice(BaseBehaviorAgent.ProFoods)        
+        while centroid in cen_pnts:
+          seed = random.randint(1,10)
+          centroid = random.Random(seed).choice(BaseBehaviorAgent.ProFoods)
+        cen_pnts.append(centroid)
+        #cen_pnts.append(random.Random(random.randint(1,500)).choice(FoodList))
+    
+    # claustering by K-means
+    self.profoodGrp0 = []
+    self.profoodGrp1 = []
+    self.profoodGrp2 = []
+    dst = []
+    n_iter = 0      # number of iteration cauld do
+    MAX_ITER = 10
+    b_converge = False
+    
+    #print cen_pnts
+    while n_iter < MAX_ITER and not b_converge:
+      for food in BaseBehaviorAgent.ProFoods:
+        # get distance b/w food and thre centroid
+        dst = [util.manhattanDistance(pnt, food) for pnt in cen_pnts]
+        min_dis = min(dst)
+        min_index = dst.index(min_dis)
+        
+        if min_index == 0:
+            if not food in self.profoodGrp0:
+                self.profoodGrp0.append(food)
+            if food in self.profoodGrp1:
+                self.profoodGrp1.remove(food)
+            if food in self.profoodGrp2:
+                self.profoodGrp2.remove(food)
+        elif min_index == 1:
+            if not food in self.profoodGrp1:
+                self.profoodGrp1.append(food)
+            if food in self.profoodGrp2:
+                self.profoodGrp2.remove(food)
+            if food in self.profoodGrp0:
+                self.profoodGrp0.remove(food)
+        elif min_index == 2:
+            if not food in self.profoodGrp2:
+                self.profoodGrp2.append(food)
+            if food in self.profoodGrp1:
+                self.profoodGrp1.remove(food)
+            if food in self.profoodGrp0:
+                self.profoodGrp0.remove(food)
+
+      #update centroid points
+      cent0x = sum([i[0] for i in self.profoodGrp0])/len(self.profoodGrp0)
+      cent0y = sum([i[1] for i in self.profoodGrp0])/len(self.profoodGrp0)
+      cent1x = sum([i[0] for i in self.profoodGrp1])/len(self.profoodGrp1)
+      cent1y = sum([i[1] for i in self.profoodGrp1])/len(self.profoodGrp1)
+      cent2x = sum([i[0] for i in self.profoodGrp2])/len(self.profoodGrp2)
+      cent2y = sum([i[1] for i in self.profoodGrp2])/len(self.profoodGrp2)
+      
+      if cen_pnts[0] == (int(cent0x), int(cent0y)) and cen_pnts[1] == (int(cent1x), int(cent1y)) and cen_pnts[2] == (int(cent2x), int(cent2y)):
+        #print "# of iter: ", n_iter+1
+        b_converge = True
+      else:
+        cen_pnts[0] = (int(cent0x), int(cent0y))
+        cen_pnts[1] = (int(cent1x), int(cent1y))
+        cen_pnts[2] = (int(cent2x), int(cent2y))
+      #print cen_pnts
+      n_iter += 1
+      
+    BaseBehaviorAgent.ProFoodGrp0 = self.profoodGrp0
+    BaseBehaviorAgent.ProFoodGrp1 = self.profoodGrp1
+    BaseBehaviorAgent.ProFoodGrp2 = self.profoodGrp2
+    for i in self.profoodGrp0:
+        self.debugDraw(i, [1,0,0])
+    for j in self.profoodGrp1:
+        self.debugDraw(j, [0,1,0])
+    for k in self.profoodGrp2:
+        self.debugDraw(k, [0,0,1])
+  
+  
   
   def food(self, successor):
     return self.getFood(successor).asList()
@@ -201,6 +309,7 @@ class BaseBehaviorAgent(CaptureAgent):
           max = value
           bestAction = action
         ghostFeature,capFeature = (0,)*2 
+    self.num_actions += 1
     return bestAction
   
   # defensive behavior - ghosts will seek and destroy invading pacman. 
@@ -225,8 +334,19 @@ class BaseBehaviorAgent(CaptureAgent):
       new_set = set(newProFoods)
       lost_food = old_set - new_set
       min_Gdst = min( [self.getMazeDistance(curPos, food) for food in lost_food] )
-      if min_Gdst != 0:
-        min_Gdst = min_Gdst**-1
+      #if min_Gdst != 0:
+      #  min_Gdst = min_Gdst**-1
+
+      # remove eaten foods from proFoodGrp
+      for f in lost_food:
+          if f in BaseBehaviorAgent.ProFoodGrp0:
+              BaseBehaviorAgent.ProFoodGrp0.remove(f)
+          elif f in BaseBehaviorAgent.ProFoodGrp1:
+              BaseBehaviorAgent.ProFoodGrp1.remove(f)
+          elif f in BaseBehaviorAgent.ProFoodGrp2:
+              BaseBehaviorAgent.ProFoodGrp2.remove(f)
+          self.debugDraw(f, [0,1,1])
+      
       BaseBehaviorAgent.ProFoods = newProFoods
     
     for action in actions:
@@ -234,19 +354,63 @@ class BaseBehaviorAgent(CaptureAgent):
       score = self.getScore(successor)
       myState = successor.getAgentState(self.index)
       myPos = myState.getPosition()
+      
+      ### update protected FoodGrp      
+      if self.cur_pro_food == None:
+          if self.cur_pro_grp == 0:
+              if not len(BaseBehaviorAgent.ProFoodGrp0) == 0:
+                  self.cur_pro_food = random.choice(BaseBehaviorAgent.ProFoodGrp0)
+              else:
+                  self.cur_pro_food = random.choice(BaseBehaviorAgent.ProFoods)
+          if self.cur_pro_grp == 1:
+              if not len(BaseBehaviorAgent.ProFoodGrp1) == 0:
+                  self.cur_pro_food = random.choice(BaseBehaviorAgent.ProFoodGrp1)
+              else:
+                  self.cur_pro_food = random.choice(BaseBehaviorAgent.ProFoods)
+          if self.cur_pro_grp == 2:
+              if not len(BaseBehaviorAgent.ProFoodGrp2) == 0:
+                  self.cur_pro_food = random.choice(BaseBehaviorAgent.ProFoodGrp2)
+              else:
+                  self.cur_pro_food = random.choice(BaseBehaviorAgent.ProFoods)
+      
+      if myPos == self.cur_pro_food:
+          self.cur_pro_grp = (self.cur_pro_grp+1) % 3   # switch to patrol other food group
+          # check if foods in group are all gone 
+          if self.cur_pro_grp == 0:
+              if not len(BaseBehaviorAgent.ProFoodGrp0) == 0:
+                  self.cur_pro_food = random.choice(BaseBehaviorAgent.ProFoodGrp0)
+              else:
+                  self.cur_pro_food = random.choice(BaseBehaviorAgent.ProFoods)
+          if self.cur_pro_grp == 1:
+              if not len(BaseBehaviorAgent.ProFoodGrp1) == 0:
+                  self.cur_pro_food = random.choice(BaseBehaviorAgent.ProFoodGrp1)
+              else:
+                  self.cur_pro_food = random.choice(BaseBehaviorAgent.ProFoods)
+          if self.cur_pro_grp == 2:
+              if not len(BaseBehaviorAgent.ProFoodGrp2) == 0:
+                  self.cur_pro_food = random.choice(BaseBehaviorAgent.ProFoodGrp2)
+              else:
+                  self.cur_pro_food = random.choice(BaseBehaviorAgent.ProFoods)
+      
+      ### add feature for food to let agent come to patrol them
+      proFoodFeature = self.getMazeDistance(myPos, self.cur_pro_food)
+      
+      
       enemies = [successor.getAgentState(i) for i in self.getOpponents(successor)]
       invaders = [a for a in enemies if a.isPacman and a.getPosition() != None]
       invaderNum = len(invaders)
       if myState.isPacman: 
         onDefense = -100
       if invaderNum > 0:
-        dist = [self.getMazeDistance(myPos, a.getPosition()) for a in invaders if a.scaredTimer != 0]
+        dist = [self.getMazeDistance(myPos, a.getPosition()) for a in invaders] #if a.scaredTimer != 0]
         if dist.__len__() > 0:
-          min_dist = (min(dist)+1)**(-1)
+          min_dist = min(dist) #(min(dist)+1)**(-1)
+      
       rev = Directions.REVERSE[gameState.getAgentState(self.index).configuration.direction]
       if action == rev: 
         reverse = 1
-      value = min_dist + -2*reverse + 100*onDefense + -1000*invaderNum + 2*min_Gdst
+      
+      value = -10*min_dist + -2*reverse + 100*onDefense + -1000*invaderNum + -10*min_Gdst - 1*proFoodFeature
       actionMap[action] = value
       if max == value:
         actionsWithSameValue = [k for k,v in actionMap.iteritems() if v == value]
@@ -259,6 +423,7 @@ class BaseBehaviorAgent(CaptureAgent):
         bestAction = action
       flag,onDefense = (1,)*2  
       reverse,min_dist = (0,)*2
+    self.num_actions += 1
     return bestAction    
   
 #------------------------------------------------------------------------------# 
@@ -269,6 +434,63 @@ class Bane(BaseBehaviorAgent):
   # chooeses the best action through a behavior depending
   # on game features
   def chooseAction(self, gameState):
-    if self.isInvading(gameState):
-      return self.defend(gameState)
-    return self.invade(gameState)
+    if self.num_actions > 20 and gameState.getAgentPosition(self.index) == self.ini_pos:
+        if BaseBehaviorAgent.pre_pole[self.index] == 'invade':        
+            BaseBehaviorAgent.num_dead[self.index] += 1
+            ## Assign role based on previous action
+            if self.index < 2:
+                if BaseBehaviorAgent.pre_pole[self.index + 2] == 'invade':       # another is invader
+                    BaseBehaviorAgent.pre_pole[self.index] = 'defend'
+                else:
+                    BaseBehaviorAgent.pre_pole[self.index] = 'defend'
+                    BaseBehaviorAgent.pre_pole[self.index + 2] = 'invade'
+            else:
+                if BaseBehaviorAgent.pre_pole[self.index - 2] == 'invade':       # another is invader
+                    BaseBehaviorAgent.pre_pole[self.index] = 'defend'
+                else:
+                    BaseBehaviorAgent.pre_pole[self.index] = 'defend'
+                    BaseBehaviorAgent.pre_pole[self.index - 2] = 'invade'
+                
+        print 'Agent: ' + str(self.index) + ' dead ' + str(BaseBehaviorAgent.num_dead)
+        
+#    if self.index < 2:
+#        return self.invade(gameState)
+#    else:
+#        return self.defend(gameState)
+    
+    if sum(BaseBehaviorAgent.num_dead) == 0:
+        if BaseBehaviorAgent.pre_pole[self.index] == None:
+            BaseBehaviorAgent.pre_pole[self.index] = 'invade'
+        return self.invade(gameState)
+
+    if BaseBehaviorAgent.pre_pole[self.index] == 'invade':
+        return self.invade(gameState)
+    else:
+        return self.defend(gameState)
+    
+    
+#    if self.index < 2 :
+#        #print 'invader agent: ', self.index
+#        if sum(BaseBehaviorAgent.num_dead) % 2 == 0:
+#            #print 'Agent: ' + str(self.index) + ' is invader'
+#            self.pre_role = 'invade'            
+#            return self.invade(gameState)
+#        else:
+#            #print 'Agent: ' + str(self.index) + ' is defender '
+#            self.pre_role = 'defend'
+#            return self.defend(gameState)
+#    else:
+#        #print 'defend agent: ', self.index
+#        if sum(BaseBehaviorAgent.num_dead) % 2 == 0:
+#            #print 'Agent: ' + str(self.index) + ' is defender '
+#            self.pre_role = 'defend'
+#            return self.defend(gameState)
+#        else:
+#            #print 'Agent: ' + str(self.index) + ' is invader '
+#            self.pre_role = 'invade'
+#            return self.invade(gameState)
+        
+#    if self.isInvading(gameState):
+#      return self.defend(gameState)
+#    return self.invade(gameState)
+
